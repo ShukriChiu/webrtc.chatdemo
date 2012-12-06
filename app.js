c31aaf2ff53d42e4cc0d2bdd9a26cebeb7d95ca2
sergio <sergio@thinkpad.(none)>
2012-12-06 22:32:10 +0800

    update

---
+++ app.js
@@ -0,0 +1,147 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    WebSocketServer = require('ws').Server,
+    chatLib = require("./chatLib"),
+    zTool = require("./zTool"),
+    EVENT_TYPE = chatLib.EVENT_TYPE,
+    PORT = chatLib.PORT,
+    onlineUserMap = new zTool.SimpleMap(),
+    historyContent = new zTool.CircleList(100),
+    connCounter = 1,
+    uid = null;
+
+var app = express();
+
+app.configure(function() {
+    app.set('port', process.env.PORT || 8000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function() {
+    app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+var server = http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+});
+wss = new WebSocketServer({
+    server: server
+});
+
+wss.on('connection', function(conn) {
+    // conn.on('open',function() {
+    //  conn.send(JSON.stringify({'uid':connCounter}));
+    // });
+    conn.on('message', function(message) {
+        var mData = chatLib.analyzeMessageData(message);
+
+        if(mData && mData.EVENT) {
+            switch(mData.EVENT) {
+            case EVENT_TYPE.LOGIN:
+                // 新用户连接
+                uid = connCounter;
+                conn.socketid = uid;
+                var newUser = {
+                    'uid': uid,
+                    'nick': chatLib.getMsgFirstDataValue(mData)
+                };
+                console.log('User:{\'uid\':' + newUser.uid + ',\'nickname\':' + newUser.nick + '}coming on protocol websocket draft ' + conn.protocolVersion);
+                console.log('current connecting counter: ' + wss.clients.length);
+                // 把新连接的用户增加到在线用户列表
+                onlineUserMap.put(uid, newUser);
+                console.log(onlineUserMap);
+                connCounter++;
+                // 把新用户的信息广播给在线用户
+                for(var i = 0; i < wss.clients.length; i++) {
+                    wss.clients[i].send(JSON.stringify({
+                        'user': onlineUserMap.get(uid),
+                        'event': EVENT_TYPE.LOGIN,
+                        'values': [newUser, onlineUserMap],
+                        'counter': connCounter
+                    }));
+                }
+                break;
+
+            case EVENT_TYPE.SPEAK:
+                // 用户发言
+                var content = chatLib.getMsgSecondDataValue(mData);
+                //同步用户发言
+                for(var i = 0; i < wss.clients.length; i++) {
+                    wss.clients[i].send(JSON.stringify({
+                        'user': onlineUserMap.get(chatLib.getMsgFirstDataValue(mData)),
+                        'event': EVENT_TYPE.SPEAK,
+                        'values': [content]
+                    }));
+                }
+                historyContent.add({
+                    'user': onlineUserMap.get(uid),
+                    'content': content,
+                    'time': new Date().getTime()
+                });
+                break;
+
+            case EVENT_TYPE.LIST_USER:
+                // 获取当前在线用户
+                conn.send(JSON.stringify({
+                    'event': EVENT_TYPE.LIST_USER,
+                    'values': onlineUserMap.values()
+                }));
+                break;
+
+            case EVENT_TYPE.LIST_HISTORY:
+                // 获取最近的聊天记录
+                conn.send(JSON.stringify({
+                    'user': onlineUserMap.get(uid),
+                    'event': EVENT_TYPE.LIST_HISTORY,
+                    'values': historyContent.values()
+                }));
+                break;
+
+            default:
+                break;
+            }
+
+        } else {
+            // 事件类型出错，记录日志，向当前用户发送错误信息
+            console.log('desc:message,userId:' + chatLib.getMsgFirstDataValue(mData) + ',message:' + message);
+            conn.send(JSON.stringify({
+                'uid': chatLib.getMsgFirstDataValue(mData),
+                'event': EVENT_TYPE.ERROR
+            }));
+        }
+    });
+    conn.on('error', function() {
+        console.log(Array.prototype.join.call(arguments, ", "));
+    });
+    conn.on('close', function() {
+        console.log('User:{\'uid\':' + conn.socketid + ',\'nickname\':' + onlineUserMap.get(conn.socketid).nick + '} logout');
+        var logoutUser = onlineUserMap.remove(conn.socketid);
+        if(logoutUser) {
+            // 把已退出用户的信息广播给在线用户
+            for(var i = 0; i < wss.clients.length; i++) {
+                wss.clients[i].send(JSON.stringify({
+                    'uid': conn.socketid,
+                    'event': EVENT_TYPE.LOGOUT,
+                    'values': [logoutUser]
+                }));
+            }
+        }
+    });
+});
+console.log('Start listening on port ' + PORT);
---
+++ public/javascripts/chat.js
@@ -0,0 +1,221 @@
+var HOST = chatLib.HOST;
+var EVENT_TYPE = chatLib.EVENT_TYPE;
+var PORT = chatLib.PORT;
+
+$(document).ready(function() {
+    var socket = null;
+    var onlineUserMap = new zTool.SimpleMap();
+    var currentUser = null;
+    var currentUserNick = null;
+    var uid = 1;
+    var connCounter = 1;
+    var flag = 0;
+
+    if(typeof WebSocket === 'undefined') {
+        $("#prePage").hide();
+        $("#errorPage").show();
+    }
+
+    // function deleteUser(id) {
+    //     console.log('come in' + id);
+    //     if(onlineUserMap.size() > 0) {
+    //         var keyset = onlineUserMap.keySet();
+    //         for(var i in keyset) {
+    //             if(onlineUserMap.get(keyset[i]).uid == id) {
+    //                 onlineUserMap.remove(keyset[i]);
+    //             }
+    //         }
+    //     }
+    // }
+
+    function updateOnlineUser() {
+        var html = ["<div>在线用户(" + onlineUserMap.size() + ")</div>"];
+        if(onlineUserMap.size() > 0) {
+            var users = onlineUserMap.values();
+            for(var i in users) {
+                html.push("<div>");
+                if(users[i].uid == currentUser.uid) {
+                    html.push("<b>" + formatUserString(users[i]) + "(我)</b>");
+                } else {
+                    html.push(formatUserString(users[i]));
+                }
+                html.push("</div>");
+            }
+        }
+
+        $("#onlineUsers").html(html.join(''));
+    }
+
+    function appendMessage(msg) {
+        $("#talkFrame").append("<div>" + msg + "</div>");
+    }
+
+    function formatUserString(user) {
+        if(!user) {
+            return '';
+        }
+        return user.nick + "<span class='gray'>(" + user.uid + ")</span> ";
+    }
+
+    function formatUserTalkString(user) {
+        return formatUserString(user) + new Date().format("hh:mm:ss") + " ";
+    }
+
+    function formatUserTalkHisString(user, time) {
+        return formatUserString(user) + new Date(time).format("yyyy-MM-dd hh:mm:ss") + " ";
+    }
+
+    function reset() {
+        if(socket) {
+            socket.close();
+        }
+        socket = null;
+        onlineUserMap = null;
+        $("#onlineUsers").html("");
+        $("#talkFrame").html("");
+        $("#nickInput").val("");
+    }
+
+    function close() {
+
+    }
+
+    $("#open").click(function(event) {
+        currentUserNick = $.trim($("#nickInput").val());
+        if('' == currentUserNick) {
+            alert('请先输入昵称');
+            return;
+        }
+        $("#prePage").hide();
+        $("#mainPage").show();
+        reset();
+
+        socket = new WebSocket("ws://" + HOST + ":" + PORT);
+        onlineUserMap = new zTool.SimpleMap();
+        socket.onmessage = function(event) {
+            var mData = chatLib.analyzeMessageData(event.data);
+
+            if(mData && mData.event) {
+                switch(mData.event) {
+                case EVENT_TYPE.LOGIN:
+                    // 新用户连接
+                    var newUser = mData.values[0];
+                    if(flag == 0) {
+                        currentUser = newUser;
+                        flag = 1;
+                    }
+                    connCounter = mData.counter;
+                    onlineUserMap.clone(mData.values[1]);
+                    updateOnlineUser();
+                    appendMessage(formatUserTalkString(newUser) + "[进入房间]");
+                    break;
+
+                case EVENT_TYPE.LOGOUT:
+                    // 用户退出
+                    var user = mData.values[0];
+                    onlineUserMap.remove(user.uid);
+                    updateOnlineUser();
+                    appendMessage(formatUserTalkString(user) + "[离开房间]");
+                    break;
+
+                case EVENT_TYPE.SPEAK:
+                    // 用户发言
+                    var content = mData.values[0];
+                    if(mData.user.uid != currentUser.uid) {
+                        appendMessage(formatUserTalkString(mData.user));
+                        appendMessage("<span>&nbsp;&nbsp;</span>" + content);
+                    }
+                    break;
+
+                case EVENT_TYPE.LIST_USER:
+                    // 获取当前在线用户
+                    var users = mData.values;
+                    if(users && users.length) {
+                        for(var i in users) {
+                            // alert(i + ' user : ' + users[i].uid);
+                            // alert('uid: ' + currentUser.uid);
+                            if(users[i].uid != currentUser.uid) onlineUserMap.put(users[i].uid, users[i]);
+                        }
+                    }
+                    //alert('currentUser:' + currentUser);
+                    updateOnlineUser();
+                    break;
+
+                case EVENT_TYPE.LIST_HISTORY:
+                    // 获取历史消息
+                    //{'user':data.user,'content':content,'time':new Date().getTime()}
+                    var data = mData.values;
+                    if(data && data.length) {
+                        for(var i in data) {
+                            appendMessage(formatUserTalkHisString(data[i].user, data[i].time));
+                            appendMessage("<span>&nbsp;&nbsp;</span>" + data[i].content);
+                        }
+                        appendMessage("<span class='gray'>==================以上为最近的历史消息==================</span>");
+                    }
+                    break;
+
+                case EVENT_TYPE.ERROR:
+                    // 出错了
+                    appendMessage("[系统繁忙...]");
+                    break;
+
+                default:
+                    break;
+                }
+
+            }
+        };
+
+        socket.onerror = function(event) {
+            appendMessage("[网络出错啦，请稍后重试...]");
+        };
+
+        socket.onclose = function(event) {
+            appendMessage("[网络连接已被关闭...]");
+            close();
+        };
+
+        socket.onopen = function(event) {
+            socket.send(JSON.stringify({
+                'EVENT': EVENT_TYPE.LOGIN,
+                'values': [currentUserNick]
+            }));
+            socket.send(JSON.stringify({
+                'EVENT': EVENT_TYPE.LIST_USER,
+                'values': [currentUserNick]
+            }));
+            socket.send(JSON.stringify({
+                'EVENT': EVENT_TYPE.LIST_HISTORY,
+                'values': [currentUserNick]
+            }));
+        };
+    });
+
+    $("#message").keyup(function(event) {
+        if(13 == event.keyCode) {
+            sendMsg();
+        }
+    });
+
+    function sendMsg() {
+        var value = $.trim($("#message").val());
+        if(value) {
+            $("#message").val('');
+            appendMessage(formatUserTalkString(currentUser));
+            appendMessage("<span>&nbsp;&nbsp;</span>" + value);
+            socket.send(JSON.stringify({
+                'EVENT': EVENT_TYPE.SPEAK,
+                'values': [currentUser.uid, value]
+            }));
+        }
+    };
+
+    $("#send").click(function(event) {
+        sendMsg();
+    });
+
+
+    function show(value) {
+        $("#response").html(value);
+    };
+});
